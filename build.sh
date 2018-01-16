9d973631a23a59dfffb1b0765120ea0d813c790a
Auston Stewart <auston@shedbuilt.net>
2018-01-15 17:01:41 -1000

    Fixing toolchain build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ mkdir -v build
 cd build
 case "$SHED_BUILDMODE" in
     toolchain)
-        if [ "$SHED_HOST" == 'toolchain' ]; then
+        if [ "$SHED_HOST" == 'toolchain' ] && [ "$SHED_TARGET" == 'native' ]; then
             CC=${SHED_TOOLCHAIN_TARGET}-gcc         \
             AR=${SHED_TOOLCHAIN_TARGET}-ar          \
             RANLIB=${SHED_TOOLCHAIN_TARGET}-ranlib  \
@@ -17,15 +17,18 @@ case "$SHED_BUILDMODE" in
             make -C ld clean || exit 1
             make -C ld LIB_PATH=/usr/lib:/lib || exit 1
             cp -v ld/ld-new /tools/bin || exit 1
-        else
+        elif [ "$SHED_TARGET" == 'toolchain' ]; then
             ../configure --prefix=/tools                  \
                          --with-sysroot=$SHED_INSTALLROOT \
                          --with-lib-path=/tools/lib       \
-                         --target=$SHED_NATIVE_TARGET     \
+                         --target=$SHED_TOOLCHAIN_TARGET  \
                          --disable-nls                    \
                          --disable-werror || exit 1
             make -j 1 || exit 1
             make DESTDIR="$SHED_FAKEROOT" install || exit 1
+        else
+            echo "Unsupported configuration for toolchain build"
+            exit 1
         fi
     ;;
     *)
