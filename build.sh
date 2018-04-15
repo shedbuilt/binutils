f5af27d7063ab0d59fc50272a7cf4841f6262c40
Auston Stewart <auston@shedbuilt.net>
2018-04-15 09:36:45 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir -v build
 cd build
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     toolchain)
-        if [ "$SHED_HOST" == 'toolchain' ] && [ "$SHED_TARGET" == 'native' ]; then
+        if [ "$SHED_BUILD_HOST" == 'toolchain' ] && [ "$SHED_BUILD_TARGET" == 'native' ]; then
             CC=${SHED_TOOLCHAIN_TARGET}-gcc         \
             AR=${SHED_TOOLCHAIN_TARGET}-ar          \
             RANLIB=${SHED_TOOLCHAIN_TARGET}-ranlib  \
@@ -13,23 +13,23 @@ case "$SHED_BUILDMODE" in
                          --with-lib-path=/tools/lib \
                          --with-sysroot || exit 1
             make -j 1 || exit 1
-            make DESTDIR="$SHED_FAKEROOT" install || exit 1
+            make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
             make -C ld clean || exit 1
             make -C ld LIB_PATH=/usr/lib:/lib || exit 1
             cp -v ld/ld-new /tools/bin || exit 1
-        elif [ "$SHED_TARGET" == 'toolchain' ]; then
+        elif [ "$SHED_BUILD_TARGET" == 'toolchain' ]; then
             ../configure --prefix=/tools                  \
-                         --with-sysroot=$SHED_INSTALLROOT \
+                         --with-sysroot=$SHED_INSTALL_ROOT \
                          --with-lib-path=/tools/lib       \
                          --target=$SHED_TOOLCHAIN_TARGET  \
                          --disable-nls                    \
                          --disable-werror || exit 1
             make -j 1 || exit 1
             if [[ $SHED_TOOLCHAIN_TARGET =~ ^aarch64-.* ]]; then
-                mkdir -v "${SHED_FAKEROOT}/tools/lib"
-                ln -sv lib "${SHED_FAKEROOT}/tools/lib64"
+                mkdir -v "${SHED_FAKE_ROOT}/tools/lib"
+                ln -sv lib "${SHED_FAKE_ROOT}/tools/lib64"
             fi
-            make DESTDIR="$SHED_FAKEROOT" install || exit 1
+            make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
         else
             echo "Unsupported configuration for toolchain build"
             exit 1
@@ -45,6 +45,6 @@ case "$SHED_BUILDMODE" in
                      --with-system-zlib || exit 1
         # PiLFS says that gold has issues with parallel jobs
         make tooldir=/usr -j 1 || exit 1
-        make DESTDIR="$SHED_FAKEROOT" tooldir=/usr install || exit 1
+        make DESTDIR="$SHED_FAKE_ROOT" tooldir=/usr install || exit 1
     ;;
 esac
