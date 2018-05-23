0f43be4377cdb5c776a9a5261677d890d83eb5ce
Auston Stewart <auston@shedbuilt.net>
2018-05-22 16:56:00 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 mkdir -v build
 cd build
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        if [ "$SHED_BUILD_HOST" == 'toolchain' ] && [ "$SHED_BUILD_TARGET" == 'native' ]; then
-            CC=${SHED_TOOLCHAIN_TARGET}-gcc         \
-            AR=${SHED_TOOLCHAIN_TARGET}-ar          \
-            RANLIB=${SHED_TOOLCHAIN_TARGET}-ranlib  \
-            ../configure --prefix=/tools            \
-                         --disable-nls              \
-                         --disable-werror           \
-                         --with-lib-path=/tools/lib \
-                         --with-sysroot &&
-            make -j $SHED_NUM_JOBS &&
-            make DESTDIR="$SHED_FAKE_ROOT" install &&
-            make -C ld clean &&
-            make -C ld LIB_PATH=/usr/lib:/lib &&
-            cp -v ld/ld-new /tools/bin || exit 1
-        elif [ "$SHED_BUILD_TARGET" == 'toolchain' ]; then
-            ../configure --prefix=/tools                  \
-                         --with-sysroot=$SHED_INSTALL_ROOT \
-                         --with-lib-path=/tools/lib       \
-                         --target=$SHED_TOOLCHAIN_TARGET  \
-                         --disable-nls                    \
-                         --disable-werror &&
-            make -j $SHED_NUM_JOBS || exit 1
-            if [[ $SHED_TOOLCHAIN_TARGET =~ ^aarch64-.* ]]; then
-                mkdir -v "${SHED_FAKE_ROOT}/tools/lib" &&
-                ln -sv lib "${SHED_FAKE_ROOT}/tools/lib64" || exit 1
-            fi
-            make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-        else
-            echo "Unsupported configuration for toolchain build"
-            exit 1
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    if [ "$SHED_BUILD_HOST" != "$SHED_NATIVE_TARGET" ] && [ "$SHED_BUILD_TARGET" == "$SHED_NATIVE_TARGET" ]; then
+        CC=${SHED_BUILD_HOST}-gcc         \
+        AR=${SHED_BUILD_HOST}-ar          \
+        RANLIB=${SHED_BUILD_HOST}-ranlib  \
+        ../configure --prefix=/tools            \
+                     --disable-nls              \
+                     --disable-werror           \
+                     --with-lib-path=/tools/lib \
+                     --with-sysroot &&
+        make -j $SHED_NUM_JOBS &&
+        make DESTDIR="$SHED_FAKE_ROOT" install &&
+        make -C ld clean &&
+        make -C ld LIB_PATH=/usr/lib:/lib &&
+        cp -v ld/ld-new /tools/bin || exit 1
+    elif [ "$SHED_BUILD_TARGET" != "$SHED_NATIVE_TARGET" ]; then
+        ../configure --prefix=/tools                   \
+                     --with-sysroot=$SHED_INSTALL_ROOT \
+                     --with-lib-path=/tools/lib        \
+                     --target=$SHED_BUILD_TARGET       \
+                     --disable-nls                     \
+                     --disable-werror &&
+        make -j $SHED_NUM_JOBS || exit 1
+        if [[ $SHED_BUILD_TARGET =~ ^aarch64-.* ]]; then
+            mkdir -v "${SHED_FAKE_ROOT}/tools/lib" &&
+            ln -sv lib "${SHED_FAKE_ROOT}/tools/lib64" || exit 1
         fi
-    ;;
-    *)
-        ../configure --prefix=/usr       \
-                     --enable-gold       \
-                     --enable-ld=default \
-                     --enable-plugins    \
-                     --enable-shared     \
-                     --disable-werror    \
-                     --with-system-zlib &&
-        make tooldir=/usr -j $SHED_NUM_JOBS &&
-        make DESTDIR="$SHED_FAKE_ROOT" tooldir=/usr install || exit 1
-    ;;
-esac
+        make DESTDIR="$SHED_FAKE_ROOT" install
+    else
+        echo "Unsupported configuration for toolchain build"
+        exit 1
+    fi
+else
+    ../configure --prefix=/usr       \
+                 --enable-gold       \
+                 --enable-ld=default \
+                 --enable-plugins    \
+                 --enable-shared     \
+                 --disable-werror    \
+                 --enable-64-bit-bfd \
+                 --with-system-zlib &&
+    make tooldir=/usr -j $SHED_NUM_JOBS &&
+    make DESTDIR="$SHED_FAKE_ROOT" tooldir=/usr install
+fi
